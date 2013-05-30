9157b7e32d22b2b3d74e7bcca0bc19f76259d11f
Sergey Shuvalov <swim@live.ru>
2013-05-30 10:27:13 +0400

    data

---
+++ Afisha.Models/FilmSession.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Afisha.Models
+{
+    class FilmSession
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public Movie Movie { get; set; }
+        public Theater Theater { get; set; }
+    }
+    }
+}
